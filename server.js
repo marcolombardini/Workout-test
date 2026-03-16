914ce515cca7d0257636db5959dac3a66532e3b7
Marco Lombardini <Marco.lombardini@gmail.com>
2026-03-16 11:04:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello from Railway!\n');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
