41c43dd3746684aa7fc4907aa9012da3b88108a4
Marco Lombardini <Marco.lombardini@gmail.com>
2026-03-16 11:07:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-});
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+})
